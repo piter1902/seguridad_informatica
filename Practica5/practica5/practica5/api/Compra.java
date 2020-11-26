3798994b0bf595d773c6188b2b03bdf4fa7fc684
Pedro Allué <pedro.tamargo.allue@gmail.com>
2020-11-26 09:06:58 +0100

    Espero que esto sea lo suficiente vulnerable

---
+++ Practica5/practica5/practica5/api/Compra.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Compra {
+
+    public static void main(String[] args) {
+        Carrito carrito = null;
+        Cuenta cuenta = null;
+        try {
+            carrito = new Carrito();
+            cuenta = new Cuenta();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Escribir el saldo en pantalla
+        int saldoActual = 0;
+        try {
+            saldoActual = cuenta.obtenerSaldo();
+            System.out.println("Tu saldo actual es:" + saldoActual);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Escribir la lista de productos con sus precios
+        String products = Tienda.muestraProductos();
+        List<String> listProducts = Arrays.asList(products.split("\n"));
+        listProducts.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                s.trim();
+            }
+        });
+        System.out.println("Lista de productos: ");
+        for (String s : listProducts) {
+            System.out.format("%s \t %s euros\n", s, Tienda.obtenerPrecioProducto(s));
+        }
+        // Pide el producto que se quiere comprar
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Qué quieres comprar?");
+        String comprar = scanner.nextLine();
+        // Comprueba que el saldo es suficiente para comprar el producto
+        int saldoCompra = Tienda.obtenerPrecioProducto(comprar);
+        if (saldoActual >= saldoCompra) {
+            // Saldo suficiente
+            try {
+                carrito.meterProducto(comprar);
+                int saldoFinal = saldoActual - saldoCompra;
+                cuenta.establecerSaldo(saldoFinal);
+                System.out.println("Tu saldo es: " + saldoFinal);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        scanner.close();
+        if (carrito != null) {
+            try {
+                carrito.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (cuenta != null) {
+            try {
+                cuenta.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
